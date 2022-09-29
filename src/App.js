3539176d8787ebec30514f2fd8b73880e5b76257
HawkingRadiation42 <shrey.209303098@muj.manipal.edu>
2022-09-29 23:28:46 +0530

    dependency installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+
 Amplify.configure(awsconfig);
 
 
